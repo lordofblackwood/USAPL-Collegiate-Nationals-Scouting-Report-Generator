41077f068935c9264c3acf95f28377ddfcb1f2eb
michaelkhalil <khalil.m@husky.neu.edu>
2020-02-20 12:19:35 -0500

    Want to add a daily chron job for this

---
+++ update_drive_reports.sh
@@ -0,0 +1,3 @@
+python read_nats_csv.py
+mv Women* "/Users/michaelkhalil/Google Drive File Stream/My Drive/Women's Scouting reports" 
+mv Men* "/Users/michaelkhalil/Google Drive File Stream/My Drive/Scouting reports because Mike doesn't understand python excel libraries"
