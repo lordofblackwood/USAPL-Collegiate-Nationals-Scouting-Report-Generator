8a804038010f392ff559336001b5465aeff45ea5
michaelkhalil <khalil.m@husky.neu.edu>
2020-02-20 13:12:14 -0500

    Now can make it a launchd job

--- update_drive_reports.sh
+++ update_drive_reports.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+curl -s -d /dev/null https://docs.google.com/spreadsheets/d/1Ch0l3LlZqS5CAjQLeBbx0oIgs9VIvVtad78QlGkZZGE/export?exportFormat=csv > cnat.csv
+dos2unix cnat.csv 2> /dev/null
 python read_nats_csv.py
-mv Women* "/Users/michaelkhalil/Google Drive File Stream/My Drive/Women's Scouting reports" 
-mv Men* "/Users/michaelkhalil/Google Drive File Stream/My Drive/Scouting reports because Mike doesn't understand python excel libraries"
+mv Women* ~/Google\ Drive\ File\ Stream/My\ Drive/Women\'s\ Scouting\ reports\ 
+mv Men*   ~/Google\ Drive\ File\ Stream/My\ Drive/Scouting\ reports\ because\ Mike\ doesn\'t\ understand\ python\ excel\ libraries
